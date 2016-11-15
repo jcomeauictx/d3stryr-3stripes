a6a302a4a94a606e7484a6037ed19df95facc091
John Comeau <jc@unternet.net>
2016-11-15 10:45:47 -0800

    took out list of useragents and PHP source for harvest.php

--- destroyer.py
+++ destroyer.py
@@ -219,37 +219,18 @@ def checkParameters():
     #Exit the script prematurely
     sys.exit(exitCode)
 
-#randint allows us to obtain an random integer between two integer values a and b: int=randint(a,b)
-from random import randint
+import random
 
 def agent():
-  """Returns a random user-agent."""
-  browsers=[
-      "Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4",
-      "Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4",
-      "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
-      "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
-      "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
-      "Mozilla/5.0 (iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
-      "Mozilla/5.0 (iPad; CPU OS 4_3_5 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8L1 Safari/6533.18.5",
-      "Mozilla/5.0 (Linux; U; en-us; KFAPWI Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Silk/3.13 Safari/535.19 Silk-Accelerated=true",
-      "Mozilla/5.0 (Linux; U; en-us; KFTHWI Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Silk/3.13 Safari/535.19 Silk-Accelerated=true",
-      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-us; Silk/1.0.141.16-Gen4_11004310) AppleWebkit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16 Silk-Accelerated=true",
-      "Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; Nexus S Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
-      "Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36",
-      "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19",
-      "Mozilla/5.0 (BB10; Touch) AppleWebKit/537.10+ (KHTML, like Gecko) Version/10.0.9.2372 Mobile Safari/537.10+",
-      "Mozilla/5.0 (Linux; Android 4.3; Nexus 10 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36",
-      "Mozilla/5.0 (Linux; U; Android 2.3; en-us; SAMSUNG-SGH-I717 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
-      "Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
-      "Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
-      "Mozilla/5.0 (Linux; Android 4.2.2; GT-I9505 Build/JDQ39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.59 Mobile Safari/537.36",
-      "Mozilla/5.0 (Linux; U; Android 2.2; en-us; SCH-I800 Build/FROYO) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
-  ]
-  #In this usage we acquire a random index of the browser array.
-  string = browsers[randint(0,len(browsers)-1)]
-  return string
+    '''
+    Returns a random user-agent.
+
+    >>> agent().startswith('Mozilla/5.0 ')
+    True
+    '''
+    with open("useragent.txt") as infile:
+        browsers = [s.rstrip() for s in infile.readlines()]
+    return random.choice(browsers)
 
 #We use time to sleep
 import time
@@ -737,50 +718,6 @@ def getToken(driver,mainWindow):
 
 def harvestTokensManually():
   print (d_()+s_("Manual Token Harvest")+lb_("Number of tokens harvested: "+str(len(captchaTokens))))
-  #We will create the harvest.php on the fly based on locale and sitekey values in config.cfg
-  htmlSource="""
-    <?php
-     $siteKey = '"""+sitekey+"""';
-     $lang = 'en';
-    ?>
-     <?php if (isset($_POST['g-recaptcha-response'])): ?>
-    <html>
-     <head>
-       <title>adidas Official Website | adidas</title>
-     </head>
-     <body>
-     <?php $token=$_POST['g-recaptcha-response']; ?>
-         <p id="token" value="<?php echo $token; ?>" style="padding: 3px; word-break: break-all; word-wrap: break-word;"><?php echo $token; ?></p>
-     <?php else: ?>
-    <html>
-     <head>
-       <title>d3stryr 3stripes Manual Token Harvesting | adidas</title>
-            <style type="text/css">
-                body {
-                    margin: 1em 5em 0 5em;
-                    font-family: sans-serif;
-                }
-                fieldset {
-                    display: inline;
-                    padding: 1em;
-                }
-            </style>
-     </head>
-     <body>
-        <p>Token Harvesting</p>
-        <form action="/harvest.php" method="post">
-            <fieldset>
-                <div class="g-recaptcha" data-sitekey="<?php echo $siteKey; ?>"></div>
-                <script type="text/javascript" src="https://www.google.com/recaptcha/api.js">
-                </script>
-                <p><input type="submit" value="Submit" id="submit"/></p>
-            </fieldset>
-        </form>
-     <?php endif; ?>
-     </body>
-    </html>"""
-  with open("harvest.php","w") as htmlFile:
-    htmlFile.write(htmlSource)
   browser=getChromeDriver(chromeFolderLocation="ChromeTokenHarvestFolder")
   url="http://"+harvestDomain+":"+phpServerPort+"/harvest.php"
   while len(captchaTokens) < numberOfTokens:
