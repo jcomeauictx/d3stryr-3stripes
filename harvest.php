10f38e97e426f18ed8f65677775a6c487b09f2e0
John Comeau <jc@unternet.net>
2016-11-15 10:33:17 -0800

    reads config file itself, so no longer needs to be written on the fly

---
+++ harvest.php
@@ -0,0 +1,44 @@
+
+    <?php
+     $config = parse_ini_file('config.cfg', true, INI_SCANNER_RAW);
+     //print_r($config);
+     $locale = $config['user']['parametersLocale'];
+     $siteKey = $config['sitekey'][$locale];
+     $lang = 'en';
+    ?>
+     <?php if (isset($_POST['g-recaptcha-response'])): ?>
+    <html>
+     <head>
+       <title>adidas Official Website | adidas</title>
+     </head>
+     <body>
+     <?php $token=$_POST['g-recaptcha-response']; ?>
+         <p id="token" value="<?php echo $token; ?>" style="padding: 3px; word-break: break-all; word-wrap: break-word;"><?php echo $token; ?></p>
+     <?php else: ?>
+    <html>
+     <head>
+       <title>d3stryr 3stripes Manual Token Harvesting | adidas</title>
+            <style type="text/css">
+                body {
+                    margin: 1em 5em 0 5em;
+                    font-family: sans-serif;
+                }
+                fieldset {
+                    display: inline;
+                    padding: 1em;
+                }
+            </style>
+     </head>
+     <body>
+        <p>Token Harvesting</p>
+        <form action="/harvest.php" method="post">
+            <fieldset>
+                <div class="g-recaptcha" data-sitekey="<?php echo $siteKey; ?>"></div>
+                <script type="text/javascript" src="https://www.google.com/recaptcha/api.js">
+                </script>
+                <p><input type="submit" value="Submit" id="submit"/></p>
+            </fieldset>
+        </form>
+     <?php endif; ?>
+     </body>
+    </html>
